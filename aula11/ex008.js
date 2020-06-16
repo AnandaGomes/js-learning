dc5ea012c9771e239292922f00ea3276cf16e1a9
AnandaGomes <anandaoliveira33@gmail.com>
2020-06-16 16:11:23 -0300

    curso js básico

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 78.2
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+
+if (vel > 60) { // uma condição simples. porque só tem uma via. não tem "se não".
+    console.log(`Você ultrapassou a velocidade permitida. Multado!`)
+} 
+
+console.log(`Dirija sempre usando cinto de segurança`)
