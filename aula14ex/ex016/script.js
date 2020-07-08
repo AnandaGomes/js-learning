0cbbfa5e30e4a7f7387658b7669a097aecb84d13
AnandaGomes <anandaoliveira33@gmail.com>
2020-07-08 18:33:19 -0300

    commit

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,1 +1,35 @@
-alert('Olá!')
+function contar () {
+  var inicio = document.getElementById('txti')
+  var fim = document.getElementById('txtf')
+  var passo = document.getElementById('txtp')
+  var res = document.getElementById('res')
+
+  if (
+    inicio.value.length == 0 ||
+    fim.value.length == 0 ||
+    passo.value.length == 0
+  ) {
+    res.innerHTML = 'Impossível contar. Preencha todos os campos.'
+  } else {
+    res.innerHTML = 'Contando... <br>'
+    var i = Number(inicio.value)
+    var f = Number(fim.value)
+    var p = Number(passo.value)
+    if (p <= 0) {
+      window.alert('Passo inválido! Considerando o passo como 1...')
+      p = 1
+    }
+    if (i < f) {
+      //contagem crescente:
+      for (var c = i; c <= f; c += p) {
+        res.innerHTML += `${c} \u{1F449}`
+      }
+    } else {
+      //contagem regressiva:
+      for (var c = i; c >= f; c -= p) {
+        res.innerHTML += `${c} \u{1F449}`
+      }
+    }
+    res.innerHTML += `\u{1f3C1}`
+  }
+}
--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,36 @@
-alert('Olá!')
+function tab () {
+  var textnum = document.getElementById('txtn')
+  var tab = document.getElementById('seltab')
+  var res = document.getElementById('res')
+  if (textnum.value.length == 0) {
+    window.alert('Digite um número válido!')
+  } else {
+    var num = Number(textnum.value)
+    var c = 1
+    /* 
+    OPÇÃO COM WHILE:
+      while (c <= 10) {
+      // cria uma opção dentro do select
+      let item = document.createElement('option')
+      item.text = `${num} x ${c} = ${num * c}`
+      // cria um filho 'item' dentro da opção
+      tab.appendChild(item)
+      c++
+    } 
+    */
+    // limpa os resultados da tabuada
+    for (c = 1; c <= 10; c++) {
+      // cria uma opção dentro do select
+      let item = document.createElement('option')
+      // vai dar um value='tab1'e etc dentro do HTML, o que pode ser útil
+      item.value = `tab${c}`
+      item.text = `${num} x ${c} = ${num * c}`
+      // cria um filho do tipo 'item' dentro da opção
+      tab.appendChild(item)
+    }
+  }
+}
+function limpaTela () {
+  var tab = document.getElementById('seltab')
+  tab.innerHTML = ''
+}
